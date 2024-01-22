bd4bc677fdcd1ce3f17d8709171219f785372d8f
Timo de Kort <dekort.timo@gmail.com>
2024-01-22 22:40:43 +0100

    feat: implement first system, hello gnoems

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_gnoems)
+        .add_systems(Update, greet_gnoems)
+        .run();
 }
+
+fn add_gnoems(mut commands: Commands) {
+    commands.spawn((Gnoem, Name("Willy".to_string())));
+    commands.spawn((Gnoem, Name("Kaulana".to_string())));
+    commands.spawn((Gnoem, Name("Buhle".to_string())));
+}
+
+fn greet_gnoems(query: Query<&Name, With<Gnoem>>) {
+    for name in &query {
+        println!("Hello {}!", name.0);
+    }
+}
+
+#[derive(Component)]
+struct Gnoem;
+
+#[derive(Component)]
+struct Name(String);
