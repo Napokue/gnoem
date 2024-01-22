e1b1ed0c00037e7acd3fafd6c6ce40c52c07a5cc
Timo de Kort <dekort.timo@gmail.com>
2024-01-22 23:41:48 +0100

    feat: add basic scene

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,53 @@
 use bevy::prelude::*;
+use bevy::render::camera::ScalingMode;
 
 pub struct HelloGnoemPlugin;
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, HelloGnoemPlugin))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(32.0).into()),
+        material: materials.add(Color::WHITE.into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb_u8(124, 144, 255).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        projection: OrthographicProjection {
+            scale: 3.0,
+            scaling_mode: ScalingMode::FixedVertical(2.0),
+            ..default()
+        }
+        .into(),
+        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
 impl Plugin for HelloGnoemPlugin {
